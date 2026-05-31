0242f629476e7ec1cb9b64f7e960be664d50d5f7
gondsachin40 <gondsachin2006@gmail.com>
2026-05-31 06:34:26 +0530

    updated routes

--- routes/accountRouter.ts
+++ routes/accountRouter.ts
@@ -2,7 +2,20 @@ import { Router, Request, Response } from "express";
 import { prisma } from '../lib/prisma';
 import z, { number, string } from 'zod';
 const account = Router();
+// 
+//A = 100
+//add50 -> A   , A = 150 1sec database down open 3 min
+// transfer A to b 100 -> A = 50 , 1 sec -> 0
+//
 
+
+
+// subtract lock 
+
+
+// add -> 100
+
+// user1 -> sachin Block 3 sec
 account.get('/balance', async (req: Request, res: Response) => {
     const id: number = req.user?.id as number;
     const result = await prisma.account.findUnique({
@@ -16,7 +29,7 @@ account.get('/balance', async (req: Request, res: Response) => {
     });
 })
 
-
+// first method 
 const transferSchema = z.object({
     toUser: z.string().min(2).max(30),
     amount: z.number().min(1)
@@ -33,40 +46,45 @@ function timer() {
     })
 }
 
+
 account.post('/add50', async (req: Request, res: Response) => {
     const data = 50;
-    await prisma.$transaction(async (tsx) => {
+        // sachin amount
+        // 100
+        
+        
+        // wait for 3 min
+        
+        // backend 
+        // stop until email send
+        
+        // after 4 min
+        //
+        // async
+        // 1 function only to update 
 
-        const user1 = await tsx.account.findUnique({
-            where: {
-                userId: req.user?.id
-            }
-        });
-        // await timer()
-        if (!user1) {
-            return res.status(404).json({
-                msg: 'error in user'
-            })
-        };
-        const updateUser1 = await tsx.account.update({
+        // 3 - 4 function 
+        // await timer();
+
+        const updateUser1 = await prisma.account.update({
             where: {
                 userId: req.user?.id
             },
             data: {
-                balance: {
-                    increment: data
+                balance : {
+                    increment : 50
                 }
             }
         })
         res.status(200).json(updateUser1);
-
-    })
 
 })
 
 // send money from one user to another user with username
 account.post('/transfer', async (req: Request, res: Response) => {
     const result = transferSchema.safeParse(req.body);
+
+    // user1 -> user2 
     if (!result.success) {
         return res.status(411).json({
             msg: "The person you are sending money is wrong / you'r sending amount is too small"
@@ -101,6 +119,7 @@ account.post('/transfer', async (req: Request, res: Response) => {
     //deduct the amount from user1 
     let amountToSend = data.amount;
 
+    //block -> issue 
     const user1 = await prisma.account.findUnique({
         where: {
             userId: req.user?.id
