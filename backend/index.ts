7da01df92014e65dd07849b38a992a97059cc9a5
gondsachin40 <gondsachin2006@gmail.com>
2026-05-31 08:04:41 +0530

    updated frontend

--- backend/index.ts
+++ backend/index.ts
@@ -3,9 +3,10 @@ import type { Request , Response } from "express";
 import { prisma } from "./lib/prisma";
 import api from './routes';
 import middleware from './middlewares/userMiddleware';
+import cors from 'cors';
 const app = express();
 const PORT = 8080;
-
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1"  , api);
